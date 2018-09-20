5ef759f9478532ceb327f06a6f8860095265c0d7
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-09-20 14:59:16 -0600

    almost finished

--- CreditCardScanner/src/CreditCardScanner.java
+++ CreditCardScanner/src/CreditCardScanner.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 public class CreditCardScanner
 	{
@@ -5,9 +7,16 @@ public class CreditCardScanner
 		static Scanner userInputOne = new Scanner(System.in);
 		static Scanner userInputTwo = new Scanner(System.in);
 		static long sum;
+		static long validCreditCard=0;
 		static long [] creditCardNumber = new long [16];
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 		{
+			System.out.println("Would you like to enter in your own credit card number or to inport a text file?");
+			System.out.println("Press 1 to enter your own or press 2 to inport a file.");
+			long userChoice = userInputTwo.nextLong();
+			
+			if (userChoice == 1)
+				{
 			System.out.println("Enter in a credit card number.");
 			long number = userInputTwo.nextLong();
 			for (int i = 15; i>=0;i--)
@@ -18,10 +27,15 @@ public class CreditCardScanner
 			
 			alternatingDigits();
 			validator();
+				}
+			else
+				{
+				readTextFile();
+				}
 			
 		}
 		
-		public static void alternatingDigits()
+		public static void alternatingDigits() throws IOException
 			{
 				long firstNumber = 0;
 				long lastNumber = 0;
@@ -36,21 +50,44 @@ public class CreditCardScanner
 							}		
 					}
 			}
-		public static void validator()
+		
+//		public static void tellUser()
+//			{
+//				System.out.println(creditCardNumber);
+//				System.out.println("There are " + validCreditCard + " valid credit cards.");
+//			}
+		public static void validator() throws IOException 
 		{
 			sum = 0;
+			
 			for(int i = 0; i <16; i++)
 				{
 					sum = creditCardNumber[i]+sum;
 				}
-			System.out.println(sum);
+//			System.out.println(sum);
 			if (sum%10==0)
 				{
 					System.out.print("This is a valid credit card.");
+					validCreditCard++;	
 				}
 			else
 				{
 					System.out.println("This is not a valid credit card.");
+				}
+			for (int )
+//			tellUser();
+		}
+		
+		
+		
+		public static void readTextFile() throws IOException
+		{
+			Scanner file = new Scanner(new File("creditCardNumbers.txt"));
+			while (file.hasNext())
+				{
+					sum = file.nextLong();
+					alternatingDigits();
+					validator();
 				}
 		}
 	}
